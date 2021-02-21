94b44ab87470600d729723f384056aa60a029cac
Kravchuk Petr <kravchukpetr@gmail.com>
2021-02-21 18:06:02 +0300

    . Signed-off-by: Kravchuk Petr <kravchukpetr@gmail.com>

--- parse/ParseResultsDaily.py
+++ parse/ParseResultsDaily.py
@@ -1,7 +1,7 @@
 import ParseResultsLib as prl
 import os
 
-fname = os.path.dirname(os.path.realpath(__file__)).replace("\\","/") + '/' + 'config.txt'
+fname = os.path.dirname(os.path.realpath(__file__)).replace("\\","/") + '/' + 'DB.config'
 log_dir = os.path.dirname(os.path.realpath(__file__)).replace("\\","/")
 WF_ID = 2
 
